e2848d79325ef3e8c555d34db354b80f8c5f144c
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-10-06 17:59:16 +0100

    Pedir por teclado, la dimension de la matriz

--- matrices.rb
+++ matrices.rb
@@ -1,1 +1,9 @@
 puts "Practica 4 - LPP"
+
+puts "\nCual es la dimension de la matriz a calcular" 
+STDOUT.flush
+x = gets.chomp #Recoge la dimensión en un char, que se pasará a int --> x.to_i
+matriz1 = Array.new(x.to_i){Array.new(x.to_i)} #Creacion de las matrices
+matriz2 = Array.new(x.to_i){Array.new(x.to_i)}
+
+puts "\n\t ~~ Elementos de la primera matriz~~ \n"
