db701bdf99074f4a545b55212ef637f84012fbba
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-10-06 18:16:28 +0100

    Pedir por teclado los elementos de la segunda matriz

--- matrices.rb
+++ matrices.rb
@@ -3,8 +3,8 @@ puts "Practica 4 - LPP"
 puts "\nCual es la dimension de la matriz a calcular" 
 STDOUT.flush
 x = gets.chomp #Recoge la dimensión en un char, que se pasará a int --> x.to_i
-matriz1 = Array.new(x.to_i){Array.new(x.to_i)} #Creacion de las matrices
-matriz2 = Array.new(x.to_i){Array.new(x.to_i)}
+m1= Array.new(x.to_i){Array.new(x.to_i)} #Creacion de las matrices
+m2 = Array.new(x.to_i){Array.new(x.to_i)}
 
 puts "\n\t ~~ Elementos de la primera matriz~~ \n"
 for i in (0..((x.to_i)-1))
@@ -12,6 +12,16 @@ for i in (0..((x.to_i)-1))
     puts " ~Introduzca el elemento #{i+1},#{j+1}:"
     STDOUT.flush
     y = gets.chomp
-    matriz1[i][j] = y.to_f 
+    m1[i][j] = y.to_f 
   end
 end
+
+ puts "\n\t ~~ Elementos de la segunda matriz~~ \n"
+ for i in (0..((x.to_i)-1))
+    for j in (0..((x.to_i)-1))
+     puts " ~Introduzca el elemento #{i+1},#{j+1}:"
+     STDOUT.flush
+     y = gets.chomp
+     m2[i][j] = y.to_f 
+   end
+ end
