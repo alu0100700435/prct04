bd3fef62864ca2d22354dc0e1757e5dc1c3312ef
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-10-06 18:05:11 +0100

    Pedir por teclado los elementos de la primera matriz

--- matrices.rb
+++ matrices.rb
@@ -7,3 +7,11 @@ matriz1 = Array.new(x.to_i){Array.new(x.to_i)} #Creacion de las matrices
 matriz2 = Array.new(x.to_i){Array.new(x.to_i)}
 
 puts "\n\t ~~ Elementos de la primera matriz~~ \n"
+for i in (0..((x.to_i)-1))
+   for j in (0..((x.to_i)-1))
+    puts " ~Introduzca el elemento #{i+1},#{j+1}:"
+    STDOUT.flush
+    y = gets.chomp
+    matriz1[i][j] = y.to_f 
+  end
+end
