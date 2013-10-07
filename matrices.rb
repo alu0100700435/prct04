c397ac58f01a1d7be2aa708d9d5000fa7083b7de
Carlota Lázaro <alu0100698862@ull.edu.es>
2013-10-07 13:42:36 +0100

    Mejorando la practica

--- matrices.rb
+++ matrices.rb
@@ -1,5 +1,10 @@
 puts "Practica 4 - LPP"
 
+puts "\n--------------------------------------------------------------------------------- \n"
+puts "\n|\tPROGRAMA QUE REALIZA LA SUMA Y MULTIPLICACION DE MATRICES CUADRADAS\t|\n"
+puts "\n|\t\tAutoras: Raquel Alvarez Hernandez - Carlota Lazaro Hernandez\t|\n"
+puts "\n--------------------------------------------------------------------------------- \n"
+
 puts "\nCual es la dimension de la matriz a calcular" 
 STDOUT.flush
 x = gets.chomp #Recoge la dimensión en un char, que se pasará a int --> x.to_i
@@ -26,6 +31,9 @@ puts "\n\t ~~ Elementos de la segunda matriz~~ \n"
    end
  end
 
+puts "\n--------------------------------------------------------------------------------- \n"
+puts "|\t\t~~~ La matriz resultante de la suma es ~~~ \t\t\t|"
+puts "--------------------------------------------------------------------------------- \n"
 
 puts "Resultado de la suma"
 m3 = Array.new(x.to_i){Array.new(x.to_i)} #Creacion de la matriz que contendrá el resultado de la suma
@@ -38,6 +46,11 @@ end
 
 print m3
 
+puts "\n--------------------------------------------------------------------------------- \n"
+puts "|\t\t~~~ La matriz resultante de la multiplicacion es ~~~ \t\t| "
+puts "--------------------------------------------------------------------------------- \n"
+
+
 m4 = Array.new(x.to_i){Array.new(x.to_i, 0)} #Creacion de la matriz que contendrá el resultado de la multiplicacion
 
 for i in (0..((x.to_i)-1))
@@ -48,4 +61,4 @@ for i in (0..((x.to_i)-1))
 end
 
 print m4
-
+puts " /n /n "
